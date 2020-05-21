42bc2a6e2b2bef2939490ab77d387d247632f267
AntemeniucIon-11 <65721347+AntemeniucIon-11@users.noreply.github.com>
2020-05-21 17:20:27 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/*
+autor : Antiminiuc Ion
+anul : 2020
+*/
+let datastring="",operations="+";
+
+function add(x) {
+operations=x;
+operatie.innerText=x;
+
+}
+
+function radical() {
+datastring=out.value=Math.sqrt(datastring); // 
+	
+}
+function calc(){
+
+
+datastring=out.value=eval(`${f1.value}${operations}${f2.value}`);
+
+
+
+
+
+
+}
